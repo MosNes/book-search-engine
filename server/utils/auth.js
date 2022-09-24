7d34de7f9295e966890b7288edfaf2dd0438815e
MosNes <7hekarl@gmail.com>
2022-09-24 17:23:45 -0400

    refactored server/auth.js to work with GraphQL API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,9 +6,10 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  //pass in req object
+  authMiddleware: function ({req}) {
+    // allows token to be sent via  req.query or headers, or req.body
+    let token = req.body || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,7 +17,9 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      // return res.status(400).json({ message: 'You have no token!' });
+      //return original req object
+      return req;
     }
 
     // verify token and get user data out of it
@@ -25,11 +28,10 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // return res.status(400).json({ message: 'invalid token!' });
+      //return updated req object
+      return req;
     }
-
-    // send to next endpoint
-    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
