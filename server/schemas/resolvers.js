e7ba821f1e5ecd9964c3ae369b0a10f9bc57578b
MosNes <7hekarl@gmail.com>
2022-09-25 15:14:05 -0400

    added additional comments

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,6 +13,7 @@ const resolvers = {
             if (context.user) {
                 const userData = await User.findOne({ _id: context.user._id })
                     .select('-__v -password')
+                    //DO NOT USE POPULATE ON SUBDOCUMENTS
                     //.populate('savedBooks');
                 return userData;
             } else {
